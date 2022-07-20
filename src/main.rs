166e5d726c5994218e0181ee6eb7b84b6c2fd7cc
SohamGhugare <soham.ghugare@gmail.com>
2022-07-20 22:27:51 +0530

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,7 @@
-use clap::{ Parser, Subcommand, Args };
+mod cli;
+
 use std::error::Error;
 
-#[derive(Parser)]
-#[clap(about="Minimal Todo app", version="0.1.0", author="SohamGhugare")]
-struct Cli {
-    #[clap(subcommand)]
-    command: Commands
-}
-
-#[derive(Args)]
-struct Add {
-    todo: String,
-    #[clap(short, long)]
-    priority: Option<String>
-}
-
-impl Add {
-    fn new(todo: &String, priority: Priority) -> Result<(), Box<dyn Error>> {
-        add_todo(&todo, Priority::High)?;
-        Ok(())
-    }
-}
-
-#[derive(Subcommand)]
-enum Commands {
-    Add(Add),
-    List,
-}
-
-enum Priority {
-    High,
-    Medium,
-    Low
-}
-
-impl Priority {
-    fn new(priority: &String) -> Result<Priority, &str> {
-        match priority.to_lowercase().as_str() {
-            "high" => Ok(Priority::High),
-            "med" => Ok(Priority::Medium),
-            "low" => Ok(Priority::Low),
-            _ => Err("Invalid priority.")
-        }
-        
-    }
-}
-
-fn add_todo(todo: &String, priority: Priority) -> Result<(), Box<dyn Error>> {
-    Ok(())
-}
-
 fn main() -> Result<(), Box<dyn Error>> {
-    let args = Cli::parse();
-
-    match &args.command {
-        Commands::Add(Add {todo, priority}) => {
-            if let Some(v) = priority {
-                Add::new(todo, Priority::new(&v)?)?;
-            } else {
-                println!("Adding {} to list...", todo)
-            }
-        },
-        Commands::List => println!("Listing")
-    }
-
-    Ok(())
+    cli::parse_cli()
 }
